e34018b34e6f0f72b8e87a3c53c5458bd42e38a1
Sagar Gupta <sagar.ag.gupta@gmail.com>
2026-02-10 18:21:08 +0530

    Fix mongoose connection for serverless

--- config/db.js
+++ config/db.js
@@ -1,25 +1,27 @@
 const mongoose = require("mongoose");
 
-let isConnected = false; // track connection state
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) {
-    return;
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  try {
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-      bufferCommands: false,
-    });
-
-    isConnected = db.connections[0].readyState === 1;
-
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed");
-    console.error(error.message);
-    throw error;
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((mongoose) => {
+        console.log("✅ MongoDB connected");
+        return mongoose;
+      });
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
