1ff77c506d697f56e5e9e19420250d201bdd71cb
Sagar Gupta <sagar.ag.gupta@gmail.com>
2026-02-12 16:33:19 +0530

    Increase body size limit for Base64 uploads

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 
 /* Middleware */
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 /* Routes */
 app.use("/api/test", require("./routes/test.route"));
